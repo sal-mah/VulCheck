894e32057e1c5882b0ce4e6db3c8485e62ec4da9
Ripper <ripper.v2.1@gmail.com>
2026-08-26 15:50:31 +0300

    Delete VulCheck-main directory

--- VulCheck-main/test_ollama.py
+++
@@ -1,8 +0,0 @@
-from modules.ollama_analyzer import check_ollama, list_models
-
-print("Ollama reachable:", check_ollama())
-
-if check_ollama():
-    print("Available models:")
-    for model in list_models():
-        print(f"  - {model}")
